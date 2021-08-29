681fd8de48a974bef1cce5852166a69294c630e4
Cormac Relf <web@cormacrelf.net>
2021-08-29 19:02:19 +1000

    Add a forward(field_name) arg

--- miette-derive/src/diagnostic.rs
+++ miette-derive/src/diagnostic.rs
@@ -1,9 +1,10 @@
 use proc_macro2::TokenStream;
-use quote::{format_ident, quote};
+use quote::quote;
 use syn::{punctuated::Punctuated, DeriveInput, Token};
 
 use crate::code::Code;
 use crate::diagnostic_arg::DiagnosticArg;
+use crate::forward::{Forward, WhichFn};
 use crate::help::Help;
 use crate::severity::Severity;
 use crate::snippets::Snippets;
@@ -40,6 +41,7 @@ pub struct DiagnosticConcreteArgs {
     pub help: Option<Help>,
     pub snippets: Option<Snippets>,
     pub url: Option<Url>,
+    pub forward: Option<Forward>,
 }
 
 impl DiagnosticConcreteArgs {
@@ -53,10 +55,14 @@ impl DiagnosticConcreteArgs {
         let mut severity = None;
         let mut help = None;
         let mut url = None;
+        let mut forward = None;
         for arg in args {
             match arg {
                 DiagnosticArg::Transparent => {
                     return Err(syn::Error::new_spanned(attr, "transparent not allowed"));
+                }
+                DiagnosticArg::Forward(to_field) => {
+                    forward = Some(to_field);
                 }
                 DiagnosticArg::Code(new_code) => {
                     // TODO: error on multiple?
@@ -81,6 +87,7 @@ impl DiagnosticConcreteArgs {
             severity,
             snippets,
             url,
+            forward,
         };
         Ok(concrete)
     }
@@ -172,7 +179,7 @@ impl Diagnostic {
         })
     }
 
-    pub fn gen(&self) -> TokenStream {
+    pub fn gen(&self) -> syn::Result<TokenStream> {
         match self {
             Self::Struct {
                 ident,
@@ -183,51 +190,22 @@ impl Diagnostic {
                 let (impl_generics, ty_generics, where_clause) = &generics.split_for_impl();
                 match args {
                     DiagnosticDefArgs::Transparent => {
-                        if fields.iter().len() != 1 {
-                            return quote! {
-                                compile_error!("you can only use #[diagnostic(transparent)] on a struct with exactly one field");
-                            };
-                        }
-                        let field = fields
-                            .iter()
-                            .next()
-                            .expect("MIETTE BUG: thought we knew we had exactly one field");
-                        let field_name = field
-                            .ident
-                            .clone()
-                            .unwrap_or_else(|| format_ident!("unnamed"));
-                        let matcher = match fields {
-                            syn::Fields::Named(_) => quote! { let Self { #field_name } = self; },
-                            syn::Fields::Unnamed(_) => quote! { let Self(#field_name) = self; },
-                            syn::Fields::Unit => {
-                                unreachable!("MIETTE BUG: thought we knew we had exactly one field")
-                            }
-                        };
+                        let forward = Forward::for_transparent_field(fields)?;
+                        let code_method = forward.gen_struct_method(WhichFn::Code);
+                        let help_method = forward.gen_struct_method(WhichFn::Help);
+                        let url_method = forward.gen_struct_method(WhichFn::Url);
+                        let severity_method = forward.gen_struct_method(WhichFn::Severity);
+                        let snippets_method = forward.gen_struct_method(WhichFn::Snippets);
 
-                        quote! {
+                        Ok(quote! {
                             impl #impl_generics miette::Diagnostic for #ident #ty_generics #where_clause {
-                                fn code<'a>(&'a self) -> std::boxed::Box<dyn std::fmt::Display + 'a> {
-                                    #matcher
-                                    #field_name.code()
-                                }
-                                fn help<'a>(&'a self) -> std::option::Option<std::boxed::Box<dyn std::fmt::Display + 'a>> {
-                                    #matcher
-                                    #field_name.help()
-                                }
-                                fn url<'a>(&'a self) -> std::option::Option<std::boxed::Box<dyn std::fmt::Display + 'a>> {
-                                    #matcher
-                                    #field_name.url()
-                                }
-                                fn severity(&self) -> std::option::Option<miette::Severity> {
-                                    #matcher
-                                    #field_name.severity()
-                                }
-                                fn snippets(&self) -> std::option::Option<std::boxed::Box<dyn std::iter::Iterator<Item = miette::DiagnosticSnippet> + '_>> {
-                                    #matcher
-                                    #field_name.snippets()
-                                }
+                                #code_method
+                                #help_method
+                                #url_method
+                                #severity_method
+                                #snippets_method
                             }
-                        }
+                        })
                     }
                     DiagnosticDefArgs::Concrete(concrete) => {
                         let code_body = concrete.code.gen_struct();
@@ -242,7 +220,7 @@ impl Diagnostic {
                             .as_ref()
                             .and_then(|x| x.gen_struct(ident, fields));
 
-                        quote! {
+                        Ok(quote! {
                             impl #impl_generics miette::Diagnostic for #ident #ty_generics #where_clause {
                                 #code_body
                                 #help_body
@@ -250,7 +228,7 @@ impl Diagnostic {
                                 #snip_body
                                 #url_body
                             }
-                        }
+                        })
                     }
                 }
             }
@@ -265,7 +243,7 @@ impl Diagnostic {
                 let sev_body = Severity::gen_enum(variants);
                 let snip_body = Snippets::gen_enum(variants);
                 let url_body = Url::gen_enum(ident, variants);
-                quote! {
+                Ok(quote! {
                     impl #impl_generics miette::Diagnostic for #ident #ty_generics #where_clause {
                         #code_body
                         #help_body
@@ -273,7 +251,7 @@ impl Diagnostic {
                         #snip_body
                         #url_body
                     }
-                }
+                })
             }
         }
     }
--- miette-derive/src/lib.rs
+++ miette-derive/src/lib.rs
@@ -7,6 +7,7 @@ mod code;
 mod diagnostic;
 mod diagnostic_arg;
 mod fmt;
+mod forward;
 mod help;
 mod severity;
 mod snippets;
@@ -16,8 +17,8 @@ mod utils;
 #[proc_macro_derive(Diagnostic, attributes(diagnostic, snippet, highlight))]
 pub fn derive_diagnostic(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    let cmd = match Diagnostic::from_derive_input(input) {
-        Ok(cmd) => cmd.gen(),
+    let cmd = match Diagnostic::from_derive_input(input).and_then(|cmd| cmd.gen()) {
+        Ok(cmd) => cmd,
         Err(err) => return err.to_compile_error().into(),
     };
     // panic!("{:#}", cmd.to_token_stream());
